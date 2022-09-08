1b8a8dc21b7a086fd10c8b0542ac41743f53bc1e
otra <otraotrange@gmail.com>
2022-09-08 07:28:52 +0000

    Modified composer.json.

---
+++ database/migrations/2022_09_08_154711_add_user_id_to_tasks_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserIdToTasksTable extends Migration
+{
+    
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn('users');
+        });
+    }
+}
